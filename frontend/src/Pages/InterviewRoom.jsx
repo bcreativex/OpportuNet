8cb322036e3e3469303f67dfcc6227afe0eca6b9
krishna <krishnajatav504@gmail.com>
2024-09-23 13:28:04 +0530

    Initial commit

---
+++ frontend/src/Pages/InterviewRoom.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const InterviewRoom = () => {
+    return (
+      <div style={{ position: 'relative', marginTop: '46px' }}>
+      <iframe 
+          src="https://code-editor-frontend-one.vercel.app/" 
+          style={{ width: '100%', height: '95vh', border: 'none' }} 
+          title="Interview Room"
+      />
+     
+  </div>
+      )
+}
+
+export default InterviewRoom
---
+++ frontend/src/Pages/Networking.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Networking = () => {
+  return (
+    <iframe 
+            src="https://network-opal.vercel.app/" 
+            style={{ width: '100%', height: '100vh', border: 'none', marginBottom: '16px' }} 
+            title="Interview Room"
+          />
+  )
+}
+
+export default Networking
